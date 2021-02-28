96520b216b00f0ae9d76cc3bfd391acf6db60772
irfan <hakimirfan90@gmail.com>
2021-02-28 16:45:52 +0700

    add input method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("output \n"); // membuat baris baru sesudahnya
-        System.out.print("output 1");
+        System.out.println("output 1 dengan baris baru \n"); // membuat baris baru sesudahnya
+        System.out.print("output 2");
+        System.out.println("output 3");
+
+        System.out.println("Type something and submit..");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+
+        System.out.println("Output from your input = " + input);
+        scan.close();
     }
 }
