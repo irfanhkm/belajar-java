d51b8954e55f81018862bfc0e8597c05445e5018
irfan <hakimirfan90@gmail.com>
2021-02-28 12:56:48 +0700

    add out for stack empty()

--- src/StackExample.java
+++ src/StackExample.java
@@ -4,11 +4,16 @@ public class StackExample {
         Stack<Integer> iniStack = new Stack<>();
         boolean checkStack = iniStack.empty();
 
+        System.out.println("Stack empty ? " + checkStack); //Stack empty ? true
+
         iniStack.push(10);
         iniStack.push(100);
         System.out.println(iniStack); // [10,100]
         iniStack.pop(); //
         System.out.println(iniStack); // [10]
         System.out.println(iniStack.peek()); // 10
+
+        checkStack = iniStack.empty();
+        System.out.println("Stack empty ? " + checkStack); //Stack empty ? false
     }
 }
